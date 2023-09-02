c888aefaf7825ad3d4d122b8351b386d80acb0c7
shroomwastaken <xeoplay123456@gmail.com>
2023-09-02 12:38:20 +0300

    added source code and dll

---
+++ LiveSplit.AttemptsThisSession/UI/Components/AttsThisSessionComponent.cs
@@ -0,0 +1,135 @@
+﻿using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LiveSplit.UI.Components
+{
+    public class AttsThisSessionComponent : IComponent
+    {
+        // Attempt count (starts at 0)
+        public int attemptCount;
+
+        // Finished runs count (starts at 0)
+        public int finishedCount;
+
+        // This internal component does the actual heavy lifting. Whenever we want to do something
+        // like display text, we will call the appropriate function on the internal component.
+        protected InfoTextComponent InternalComponent { get; set; }
+        // This is how we will access all the settings that the user has set.
+        public AttsThisSessionSettings Settings { get; set; }
+        // This object contains all of the current information about the splits, the timer, etc.
+        protected LiveSplitState CurrentState { get; set; }
+
+        public string ComponentName => "Attempts This Session";
+
+        public float HorizontalWidth => InternalComponent.HorizontalWidth;
+        public float MinimumWidth => InternalComponent.MinimumWidth;
+        public float VerticalHeight => InternalComponent.VerticalHeight;
+        public float MinimumHeight => InternalComponent.MinimumHeight;
+
+        public float PaddingTop => InternalComponent.PaddingTop;
+        public float PaddingLeft => InternalComponent.PaddingLeft;
+        public float PaddingBottom => InternalComponent.PaddingBottom;
+        public float PaddingRight => InternalComponent.PaddingRight;
+
+        // I'm going to be honest, I don't know what this is for, but I know we don't need it.
+        public IDictionary<string, Action> ContextMenuControls => null;
+
+        // This function is called when LiveSplit creates your component. This happens when the
+        // component is added to the layout, or when LiveSplit opens a layout with this component
+        // already added.
+        public AttsThisSessionComponent(LiveSplitState state)
+        {
+            attemptCount = 0;
+            finishedCount = 0;
+
+            Settings = new AttsThisSessionSettings();
+            InternalComponent = new InfoTextComponent("Attempts This Session", attemptCount.ToString());
+
+            state.OnStart += state_OnStart;
+            state.OnSplit += state_OnSplit;
+
+            CurrentState = state;
+        }
+
+        void state_OnStart(object sender, EventArgs e)
+        {
+            attemptCount += 1;
+        }
+
+        void state_OnSplit(object sender, EventArgs e)
+        {
+            if (CurrentState.CurrentPhase.Equals(TimerPhase.Ended))
+            {
+                finishedCount += 1;
+            }
+        }
+
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+        {
+            InternalComponent.NameLabel.HasShadow
+                = InternalComponent.ValueLabel.HasShadow
+                = state.LayoutSettings.DropShadows;
+
+            InternalComponent.NameLabel.ForeColor = state.LayoutSettings.TextColor;
+            InternalComponent.ValueLabel.ForeColor = state.LayoutSettings.TextColor;
+
+            InternalComponent.DrawHorizontal(g, state, height, clipRegion);
+        }
+
+        // We will be adding the ability to display the component across two rows in our settings menu.
+        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+        {
+            InternalComponent.DisplayTwoRows = Settings.Display2Rows;
+
+            InternalComponent.NameLabel.HasShadow
+                = InternalComponent.ValueLabel.HasShadow
+                = state.LayoutSettings.DropShadows;
+
+            InternalComponent.NameLabel.ForeColor = state.LayoutSettings.TextColor;
+            InternalComponent.ValueLabel.ForeColor = state.LayoutSettings.TextColor;
+
+            InternalComponent.DrawVertical(g, state, width, clipRegion);
+        }
+
+        public Control GetSettingsControl(LayoutMode mode)
+        {
+            Settings.Mode = mode;
+            return Settings;
+        }
+
+        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+        {
+            return Settings.GetSettings(document);
+        }
+
+        public void SetSettings(System.Xml.XmlNode settings)
+        {
+            Settings.SetSettings(settings);
+        }
+
+        // This is the function where we decide what needs to be displayed at this moment in time,
+        // and tell the internal component to display it. This function is called hundreds to
+        // thousands of times per second.
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+            InternalComponent.InformationValue = finishedCount.ToString() + "/" + attemptCount.ToString();
+            InternalComponent.Update(invalidator, state, width, height, mode);
+        }
+
+        // This function is called when the component is removed from the layout, or when LiveSplit
+        // closes a layout with this component in it.
+        public void Dispose()
+        {
+            CurrentState.OnStart -= state_OnStart;
+            CurrentState.OnSplit -= state_OnSplit;
+        }
+
+        // I do not know what this is for.
+        public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
+    }
+}
